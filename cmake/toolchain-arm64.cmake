4a299b9da5e5726c9b36d0d35cd4f1dae62e6779
HighCrit <35771251+HighCrit@users.noreply.github.com>
2026-06-07 00:15:59 +0000

    Pregenerate arm64 toolchain

---
+++ cmake/toolchain-arm64.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_C_COMPILER   aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
+
+set(CMAKE_C_FLAGS_INIT   -march=armv8-a)
+set(CMAKE_CXX_FLAGS_INIT -march=armv8-a)
+
+# Only search the target sysroot for libraries/headers, never the host paths
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
